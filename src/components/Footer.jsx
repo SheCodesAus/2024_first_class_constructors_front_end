c78aee92d87e8ae1ef050f045f7e4de37c916f29
cristianetanaka <147505024+cristianetanaka@users.noreply.github.com>
2024-03-24 23:18:39 +0800

    styling header navbar and header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,12 @@
 
 function Footer() {
     return (
-        //className="/*add css style from tailwind/"
       <div className="">
-    <h3>JJJJJ</h3>
-    <p></p>
+      <h1
+        className="text-3xl font-bold text-center text-primary-300 font-fredoka text-h1"
+      >
+        This is the Footer.
+      </h1>
     </div>
     )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,12 +7,19 @@ function NavBar() {
         <img
           src="src/assets/Images/GIFT_COMPARE_LOGO.svg"
           alt="Gift compare"
-          className="w-auto h-16"
+          className="w-max-w-lg h-16"
         />
-      </div>
-      <div>
+     </div>  
+      <div className="m-4 flex flex-col items-center">
+        <img
+          src="src/assets/Images/A_ good_gift_makes_every_moment_happy.svg"
+          alt="Gift compare"
+          className="w-44 h-18"
+        />
+    </div>
+    <div>
         <nav>
-          <ul className="flex flex-row space-x-4 justify-center m-6">
+          <ul className="flex flex-row space-x-3 justify-center m-3">
             <li><NavLink to="/">HOME</NavLink></li>
             <li>TRENDING</li>
             <li>WEDDING</li>
