f27d76dbad988a6ab076af9dfd3679d6d1d38e05
Sandra  Lopez <sandralopez@Sandras-MacBook-Pro-2.local>
2024-03-25 14:53:54 +0800

    modified dummy data images to test rage of images specs responsiveness

--- src/data.js
+++ src/data.js
@@ -40,7 +40,7 @@ export const allGifts= [
         "name": "AMAZING GIFT 1",
         "description": "Description for Gift 1",
         "price": "$29.99",
-        "img":  "src/assets/Images/christmas-xmas-gifts-presents.webp",
+        "img":  "https://images.pexels.com/photos/378007/pexels-photo-378007.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         "featured_time": "2024-03-23T20:00:00+08:00",
         "source_url": "https://example.com",
         "categories": [1]
@@ -50,7 +50,7 @@ export const allGifts= [
         "name": "AMAZING GIFT 2",
         "description": "Description for Gift 2",
         "price": "39.99",
-        "img": "src/assets/Images/bow_asset2.png",
+        "img": "https://images.pexels.com/photos/1809344/pexels-photo-1809344.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         "featured_time": "2024-03-24T20:00:00+08:00",
         "source_url": "https://example.com",
         "categories": [2]
@@ -70,7 +70,7 @@ export const allGifts= [
         "name": "Gift 4",
         "description": "Description for Gift 4",
         "price": "59.99",
-        "img": "https://picsum.photos/id/237/200/300",
+        "img": "https://images.pexels.com/photos/3309878/pexels-photo-3309878.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         "featured_time": "2024-03-26T20:00:00+08:00",
         "source_url": "https://example.com",
         "categories": [4, 5]
@@ -80,7 +80,7 @@ export const allGifts= [
         "name": "Gift 5",
         "description": "Description for Gift 5",
         "price": "69.99",
-        "img": "https://picsum.photos/id/237/200/300",
+        "img": "https://images.pexels.com/photos/264917/pexels-photo-264917.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         "featured_time": "2024-03-27T20:00:00+08:00",
         "source_url": "https://example.com",
         "categories": [1, 2]
@@ -90,7 +90,7 @@ export const allGifts= [
         "name": "Gift 6",
         "description": "Description for Gift 6",
         "price": "79.99",
-        "img": "https://picsum.photos/id/237/200/300",
+        "img": "https://images.pexels.com/photos/1765496/pexels-photo-1765496.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         "featured_time": "2024-03-28T20:00:00+08:00",
         "source_url": "https://example.com",
         "categories": [2, 3]
@@ -100,7 +100,7 @@ export const allGifts= [
         "name": "Gift 7",
         "description": "Description for Gift 7",
         "price": "89.99",
-        "img": "https://picsum.photos/id/237/200/300",
+        "img": "https://images.pexels.com/photos/5938414/pexels-photo-5938414.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         "featured_time": "2024-03-29T20:00:00+08:00",
         "source_url": "https://example.com",
         "categories": [3, 4]
@@ -110,7 +110,7 @@ export const allGifts= [
         "name": "Gift 8",
         "description": "Description for Gift 8",
         "price": "99.99",
-        "img": "https://picsum.photos/id/237/200/300",
+        "img": "https://images.pexels.com/photos/6045704/pexels-photo-6045704.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         "featured_time": "2024-03-30T20:00:00+08:00",
         "source_url": "https://example.com",
         "categories": [4, 5]
@@ -120,7 +120,7 @@ export const allGifts= [
         "name": "Gift 9",
         "description": "Description for Gift 9",
         "price": "109.99",
-        "img": "https://picsum.photos/id/237/200/300",
+        "img": "https://images.pexels.com/photos/4041391/pexels-photo-4041391.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         "featured_time": "2024-03-31T20:00:00+08:00",
         "source_url": "https://example.com",
         "categories": [1, 5]
@@ -130,7 +130,7 @@ export const allGifts= [
         "name": "Gift 10",
         "description": "Description for Gift 10",
         "price": "119.99",
-        "img": "https://picsum.photos/id/237/200/300",
+        "img": "https://images.pexels.com/photos/7319294/pexels-photo-7319294.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         "featured_time": "2024-04-01T20:00:00+08:00",
         "source_url": "https://example.com",
         "categories": [2, 4]
@@ -140,7 +140,7 @@ export const allGifts= [
         "name": "Gift 11",
         "description": "Description for Gift 11",
         "price": "129.99",
-        "img": "https://picsum.photos/id/237/200/300",
+        "img": "https://images.pexels.com/photos/7358301/pexels-photo-7358301.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         "featured_time": "2024-04-02T20:00:00+08:00",
         "source_url": "https://example.com",
         "categories": [3, 5]
@@ -150,7 +150,7 @@ export const allGifts= [
         "name": "Gift 12",
         "description": "Description for Gift 12",
         "price": "139.99",
-        "img": "https://picsum.photos/id/237/200/300",
+        "img": "https://images.pexels.com/photos/6708774/pexels-photo-6708774.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         "featured_time": "2024-04-03T20:00:00+08:00",
         "source_url": "https://example.com",
         "categories": [1, 2, 3]
@@ -160,7 +160,7 @@ export const allGifts= [
         "name": "Gift 13",
         "description": "Description for Gift 13",
         "price": "149.99",
-        "img": "https://picsum.photos/id/237/200/300",
+        "img": "https://images.pexels.com/photos/2072148/pexels-photo-2072148.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         "featured_time": "2024-04-04T20:00:00+08:00",
         "source_url": "https://example.com",
         "categories": [2, 4, 5]
@@ -170,7 +170,7 @@ export const allGifts= [
         "name": "Gift 14",
         "description": "Description for Gift 14",
         "price": "159.99",
-        "img": "https://picsum.photos/id/237/200/300",
+        "img": "https://images.pexels.com/photos/3856052/pexels-photo-3856052.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         "featured_time": "2024-04-05T20:00:00+08:00",
         "source_url": "https://example.com",
         "categories": [1, 3, 5]
@@ -180,7 +180,7 @@ export const allGifts= [
         "name": "Gift 15",
         "description": "Description for Gift 15",
         "price": "169.99",
-        "img": "https://picsum.photos/id/237/200/300",
+        "img": "https://images.pexels.com/photos/7694726/pexels-photo-7694726.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         "featured_time": "2024-04-06T20:00:00+08:00",
         "source_url": "https://example.com",
         "categories": [1, 5]
@@ -190,7 +190,7 @@ export const allGifts= [
         "name": "Gift 16",
         "description": "Description for Gift 16",
         "price": "179.99",
-        "img": "https://picsum.photos/id/237/200/300",
+        "img": "https://images.pexels.com/photos/4482932/pexels-photo-4482932.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         "featured_time": "2024-04-07T20:00:00+08:00",
         "source_url": "https://example.com",
         "categories": [1, 2, 4]
@@ -200,7 +200,7 @@ export const allGifts= [
         "name": "Gift 17",
         "description": "Description for Gift 17",
         "price": "189.99",
-        "img": "https://picsum.photos/id/237/200/300",
+        "img": "https://images.pexels.com/photos/6306057/pexels-photo-6306057.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         "featured_time": "2024-04-08T20:00:00+08:00",
         "source_url": "https://example.com",
         "categories": [2, 3, 5]
@@ -210,7 +210,7 @@ export const allGifts= [
         "name": "Gift 18",
         "description": "Description for Gift 18",
         "price": "199.99",
-        "img": "https://picsum.photos/id/237/200/300",
+        "img": "https://images.pexels.com/photos/10278973/pexels-photo-10278973.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         "featured_time": "2024-04-09T20:00:00+08:00",
         "source_url": "https://example.com",
         "categories": [1, 4, 5]
@@ -220,7 +220,7 @@ export const allGifts= [
         "name": "Gift 19",
         "description": "Description for Gift 19",
         "price": "209.99",
-        "img": "https://picsum.photos/id/237/200/300",
+        "img": "src/assets/Images/christmas-xmas-gifts-presents.webp",
         "featured_time": "2024-04-10T20:00:00+08:00",
         "source_url": "https://example.com",
         "categories": [1, 2, 3, 4]
@@ -230,7 +230,7 @@ export const allGifts= [
         "name": "Gift 20",
         "description": "Description for Gift 20",
         "price": "219.99",
-        "img": "https://picsum.photos/id/237/200/300",
+        "img": "https://images.pexels.com/photos/6029368/pexels-photo-6029368.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         "featured_time": "2024-04-11T20:00:00+08:00",
         "source_url": "https://example.com",
         "categories": [1, 3, 4, 5]
