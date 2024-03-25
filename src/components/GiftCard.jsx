7f7e595d9d868670c7dcf549a2df203e77a5bf25
Sandra  Lopez <sandralopez@Sandras-MacBook-Pro-2.local>
2024-03-25 13:25:06 +0800

    amend css style + responsiveness/comare button logic to save gift item in local storage

--- src/components/GiftCard.jsx
+++ src/components/GiftCard.jsx
@@ -1,22 +1,51 @@
+import React from "react";
 import { Link } from "react-router-dom";
-import Button from "./button";
+import CompareButton from "./CompareButton";
 
 function GiftCard(props) {
   const { giftData } = props;
 
+  const handleAddGift = () => {
+    // Retrieve existing gifts from localStorage or initialize as an empty array
+    const existingGifts = JSON.parse(localStorage.getItem("gifts")) || [];
+
+    // Add the current giftData to the existing gifts array
+    const updatedGifts = [...existingGifts, giftData];
+
+    // Save the updated gifts array to localStorage
+    localStorage.setItem("gifts", JSON.stringify(updatedGifts));
+
+    alert("Gift added to compare list!");
+    console.log(updatedGifts)
+  };
+
   return (
-    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4">
-      <Link to="/GiftDetailPage" className="block">
-        <div className="relative overflow-hidden bg-white border border-black" style={{ paddingBottom: '100%' }}> 
-          <img src={giftData.img} alt={giftData.name} className="absolute inset-0 object-cover w-full h-full" /> 
-        </div>
-        <div className="mt-2">
-          <h3 className="font-montserrat text-h3 tracking-wider ">{giftData.name}</h3>
-          <h3 className="mt-2 font-montserrat text-h3 font-bold tracking-wider">{giftData.price}</h3>
-        </div>
-      </Link>
-      
-      <Button text="ADD TO COMPARE LIST" link="/compare" />
+    <div className="w-auto mx-4 my-8 p-4 sm:w-1/2 md:w-1/3 lg:w-1/4 flex flex-col">
+      <div>
+        <Link to="/GiftDetailPage">
+          <div
+            className="rounded-md relative overflow-hidden bg-white border-[1px]  border-primary-600 border-opacity-80"
+            style={{ paddingBottom: "100%" }}
+          >
+            <img
+              src={giftData.img}
+              alt={giftData.name}
+              className="absolute inset-0 object-cover w-full h-full"
+            />
+          </div>
+        </Link>
+      </div>
+      <div className="my-3 space-y-2">
+        <h3 className="font-montserrat text-primary-100 text-h3 tracking-wider uppercase">
+          {giftData.name}
+        </h3>
+        <h3 className="font-montserrat  text-primary-100  text-[22px] tracking-wider">
+          {giftData.price}
+        </h3>
+      </div>
+      <div>
+        <CompareButton onClick={handleAddGift} />
+      </div>
     </div>
   );
 }
