fb5fb4fef917b944288d8e897940c40c2264707d
Sandra  Lopez <sandralopez@Sandras-MacBook-Pro-2.local>
2024-03-25 20:55:47 +0800

    Birthday + Anniversary section added to homepage - respective category pages done too. Link working. Not yet connected to nav

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,12 +4,18 @@ import { allGifts } from "../data";
 
 // Import Home page components Trending, budget, Birthday, Anniversary
 import TrendingSection from "../components/TrendingSection";
+// import BudgetSection from "../components/BudgetSection";
+import BirthdaySection from "../components/BirthdaySection";
+import AnniversarySection from "../components/AnniversarySection";
 
 export default function HomePage() {
   return (
-    <div className="m-10">
-      <div>
+    <div className="m-12">
+      <div className="flex flex-col space-y-20">
         <TrendingSection />
+        {/* <BudgetSection /> */}
+        <BirthdaySection />
+        <AnniversarySection />
       </div>
     </div>
   );
