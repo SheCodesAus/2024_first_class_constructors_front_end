e3efed98929ab03150287cdc45ce9843ea145d5c
Anastasiia Mospan <mospansha@gmail.com>
2024-03-29 11:29:29 +0800

    comming branch to be merged with DEV. Using dummy data - can access gift/1

--- src/pages/GiftDetailPage.jsx
+++ src/pages/GiftDetailPage.jsx
@@ -33,47 +33,48 @@ function GiftDetailPage() {
   };
 
   return (
-    <div className="flex flex-col justify-center align-center space-y-8 m-12 w-300 ">
-    <div className="bg-[url('src/assets/Images/pexels-photo-6331088.jpeg')] bg-cover bg-center flex justify-center items-center h-full">
-      <div className="bg-primary-500 px-6 my-14 md:mx-40 md:px-10 lg:px-20">
-      <h1 className="font-fredoka text-slate-100 text-h1 text-center tracking-wider uppercase px-1">
-        HAPPY SHOPPING
-      </h1>
-      </div>
-    </div>
-   
-    <div className="flex justify-center items-center min-h-screen  w-300 ">
-
-      <div className=" bg-pink-400 bg-opacity-10 rounded-md overflow-hidden shadow-lg p-20 border border-primary-600 border-opacity-80 flex flex-row justify-start items-center">
-       
-        <div className="w-2/5 flex flex-col justify-start">
-          <img src={oneGift.img} alt={oneGift.name} className="object-cover w-full h-full rounded-md overflow-hidden shadow-lg  border border-primary-600 border-opacity-80" />
+    <div className="flex justify-center items-center m-12">
+      <div className="w-300">
+        <div className="bg-[url('src/assets/Images/pexels-photo-6331088.jpeg')] bg-cover bg-center flex justify-center items-center h-full">
+          <div className="bg-primary-500 px-6 my-14 md:mx-40 md:px-10 lg:px-20">
+            <h1 className="font-fredoka text-slate-100 text-h1 text-center tracking-wider uppercase px-1">
+              HAPPY SHOPPING
+            </h1>
+          </div>
         </div>
+        
+        <div className="flex mt-20 max-w-[1000px]">
+  <div className="p-20 mb-12 flex flex-row justify-start items-start w-full h-full bg-pink-400 bg-opacity-10 rounded-md overflow-hidden shadow-lg border border-primary-600 border-opacity-80">
+    <div className="flex-shrink-0 w-2/5">
+    <img src={oneGift.img} alt={oneGift.name} className="object-cover  rounded-md overflow-hidden shadow-lg border border-primary-600 border-opacity-80" />
+    </div>
 
-        <div className="w-full md:w-3/5 flex flex-col m-10 justify-start ">
-          <div>
-            <h3 className="font-montserrat text-primary-100 text-h3 tracking-wider uppercase">
-              {oneGift.name}
-            </h3>
-            <p className="font-montserrat text-primary-100 text-h3 tracking-wider uppercase">
-              {oneGift.description}
-            </p>
-            <h3 className="font-montserrat text-primary-100 text-[22px] tracking-wider">
-              {oneGift.price}
-            </h3>
-          </div>
-           <div className="flex flex-col justify-end">
-          <div>
-            <CompareButton onClick={handleAddGift} />
-          </div>
-          <button className="flex justify-center border rounded-md bg-white border-primary-200 text-primary-200 hover:bg-primary-200 hover:text-white font-montserrat text-h3 uppercase text-center my-2 w-full p-4 backdrop:tracking-wider">
-            BUY ITEM HERE
-          </button>
-          </div>
+    <div className="w-full md:w-3/5 flex flex-col m-12 justify-between">
+      <div>
+        <h3 className="font-fredoka text-primary-100 text-h1 tracking-wider uppercase font-semibold mt-2">
+          {oneGift.name}
+        </h3>
+        <p className="font-montserrat text-primary-100 text-h2 tracking-wider uppercase  mt-2">
+          {oneGift.description}
+        </p>
+        <h3 className="font-montserrat text-primary-100 text-[22px] tracking-wider mt-3">
+          {oneGift.price}
+        </h3>
+      </div>
 
+              <div className=" mt-auto">
+                <div>
+                  <CompareButton onClick={handleAddGift} />
+                </div>
+                <button className="flex justify-center border rounded-md bg-white border-primary-200 text-primary-200 hover:bg-primary-200 hover:text-white font-montserrat text-h3 uppercase text-center my-2 w-full p-4 backdrop:tracking-wider">
+                  BUY ITEM HERE
+                </button>
+              </div>
+
+            </div>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 }
