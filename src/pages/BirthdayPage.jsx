e02b3a133e9242832236a14112a2d745628e5004
alisha <github@ahills.co>
2024-03-29 21:15:33 +0800

    updated BirthdayPage to pull data from api, added error and loading states and fixed typo in use-gifts

--- src/pages/BirthdayPage.jsx
+++ src/pages/BirthdayPage.jsx
@@ -2,22 +2,27 @@ import React from "react";
 import { useState, useEffect } from 'react';
 import useGifts from "../hooks/use-gifts";
 
-// import { allGifts } from "../data";
-
 import GiftCard from "../components/GiftCard";
 
 function BirthdayPage() {
   const { gifts, isLoading, error } = useGifts();
-  // const [errorMessage, setErrorMessage] = useState(null);
-
-  // Get the last 4 latest gifts as featured for now - TO BE UPDATED FEATURED TIME!
-  const BirthdayGifts = gifts.filter((gift) =>
-  gift.categories.includes(1)
-  );
+  const [errorMessage, setErrorMessage] = useState();
 
   
+  // Get the last 4 latest gifts as featured for now - TO BE UPDATED FEATURED TIME!
+  const BirthdayGifts = gifts.filter((gift) =>
+    gift.categories.includes(1)
+  );
+
+  if (isLoading) {
+    return (<p>Loading</p>)
+  }
+
+  if (error) {
+      return (<p>{error.message}</p>)
+  }
+  
   return (
-    
     <div className="flex flex-col justify-center align-center space-y-8 m-12">
       <div className="bg-[url('src/assets/Images/pexels-photo-796605.webp')] bg-cover bg-center flex justify-center items-center h-full">
         <div className="bg-primary-300 px-6 my-14 md:mx-40 md:px-10 lg:px-20">
@@ -30,7 +35,7 @@ function BirthdayPage() {
       <div className="flex flex-wrap md:mx-6 lg:mx-20">
         {BirthdayGifts.map((giftData, key) => (
           <div className="w-full sm:w-1/2 md:w-1/3">
-            <GiftCard key={key} giftData={giftData} />
+            <GiftCard key={giftData.id} giftData={giftData} />
           </div>
         ))}
       </div>
