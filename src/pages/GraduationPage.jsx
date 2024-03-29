7b19e22a73eb979c4fb905b69186d181a58b4c45
Sandra  Lopez <sandralopez@Sandras-MacBook-Pro-2.local>
2024-03-29 16:12:17 +0800

    fixed blue heading grad

--- src/pages/GraduationPage.jsx
+++ src/pages/GraduationPage.jsx
@@ -18,7 +18,7 @@ function GraduationPage() {
   return (
     <div className="flex flex-col justify-center align-center space-y-8 m-12">
       <div className="bg-[url('src/assets/Images/pexels-photo-4303015.jpeg')] bg-cover bg-center flex justify-center items-center h-full">
-        <div className="bg-primary-500 px-6 my-14 md:mx-40 md:px-10 lg:px-20">
+        <div className="bg-primary-300 px-6 my-14 md:mx-40 md:px-10 lg:px-20">
         <h1 className="font-fredoka  text-slate-100 text-h1 text-center tracking-wider uppercase px-1">
           Graduation
         </h1>
