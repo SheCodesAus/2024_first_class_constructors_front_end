4d75a9d09a158056668a74d140fa3ba1cd0f8c99
lruizper <lruizper@gmail.com>
2024-03-30 11:38:19 +0800

    Edited GiftComparisonCard component to include buy and remove buttons

--- src/components/Comparison.jsx
+++ src/components/Comparison.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react';
+
 import GiftComparisonCard from "./GiftComparisonCard";
 
 function getGiftsFromLocalStorage() {
@@ -30,7 +31,7 @@ function Comparison() {
             <div className="bg-[url('src/assets/Images/photo-1513151233558-d860c5398176.jpeg')] bg-cover bg-center flex justify-center items-center h-full">
                 <div className="bg-primary-300 px-6 my-14 md:mx-40 md:px-10 lg:px-20">
                     <h1 className="font-fredoka text-slate-100 text-h1 text-center tracking-wider uppercase px-1">
-                        Compare your options
+                        Your picks
                     </h1>
                 </div>
             </div>
--- src/components/GiftComparisonCard.jsx
+++ src/components/GiftComparisonCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import CompareButton from "./CompareButton";
 
-function GiftCard(props) {
+function GiftComparisonCard(props) {
   const { giftData } = props;
 
   return (
@@ -28,10 +28,19 @@ function GiftCard(props) {
         <h3 className="font-montserrat  text-primary-100  text-[22px] tracking-wider">
           {giftData.price}
         </h3>
+        <p className="text-primary-100 tracking-wider mt-2">
+          {giftData.description}
+        </p>
+        <button className="flex justify-center border rounded-md bg-white border-primary-200 text-primary-200 hover:bg-primary-200 hover:text-white font-montserrat text-h3 uppercase text-center my-2 w-full p-4 backdrop:tracking-wider">
+          BUY ITEM HERE
+        </button>
+        <button className="flex justify-center border rounded-md bg-white border-primary-200 text-primary-200 hover:bg-primary-200 hover:text-white font-montserrat text-h3 uppercase text-center my-2 w-full p-4 backdrop:tracking-wider">
+          REMOVE
+        </button>
       </div>
     </div>
   );
 }
 
 
-export default GiftCard;
+export default GiftComparisonCard;
