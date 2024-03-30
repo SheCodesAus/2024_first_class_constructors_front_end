aaeb01464991c725bc651740df16c3980ee118ee
lruizper <lruizper@gmail.com>
2024-03-30 10:27:18 +0800

    gift icon directing to compare page

---
+++ src/pages/ComparePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import { useState, useEffect } from 'react';
+// import useGifts from "../hooks/use-gifts";
+
+function ComparePage() {
+    return (
+        <div>
+            <h1>Compare Page</h1>
+        </div>
+    );
+}
+export default ComparePage;
