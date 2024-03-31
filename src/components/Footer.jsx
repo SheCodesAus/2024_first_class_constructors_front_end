51f3b2e578fbac9661e184de458f5de2bdd1c28d
cristianetanaka <147505024+cristianetanaka@users.noreply.github.com>
2024-04-01 00:47:52 +0800

    adjusting Gift logo and background still needs final check

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,13 +8,13 @@ function Footer() {
           className=""
         />
         {/* This needs to be fixed responsivness */}
-        <div className="absolute top-24 left-24 right-24 bottom-12 lg:top-60 lg:left-60 lg:right-60 lg:bottom-40 flex flex-col justify-center items-center bg-slate-100 gap-2 xl:my-20 xl:mx-60">
+        <div className="absolute top-24 left-24 right-24 bottom-12 lg:top-60 lg:left-60 lg:right-60 lg:bottom-40 flex flex-col justify-center items-center bg-slate-100 gap-2 flex-auto mt-40 xl:my-60 xl:mx-80">
           <img
             src="src/assets/Images/GIFT_COMPARE_LOGO.svg"
             alt="Footer Logo"
-            className="w-40 h-auto bg-slate-100 px-10 py-4"
+            className="flex w-40 h-auto bg-slate-100 px-30 py-10"
           />
-          <p className="font-fredoka text-[14px] text-primary-200">Thank you for comparing your gifts with us!</p>
+          <p className="font-fredoka text-[12px] text-primary-200">Thank you for comparing your gifts with us!</p>
           <p className="font-montserrat text-[10px] text-primary-200 mb-2">Gift Compare Pty Ltd 2024</p>
         </div>
       </div>
