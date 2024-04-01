07ea90fd2202bdb70d1dc934817f70b479895830
Anastasiia Mospan <mospansha@gmail.com>
2024-04-01 21:39:56 +0800

    adjusted styling for medium screen

--- src/components/GiftComparisonCard.jsx
+++ src/components/GiftComparisonCard.jsx
@@ -6,41 +6,35 @@ function GiftComparisonCard(props) {
   const { giftData } = props;
 
   return (
-    <div className=" border  border-primary-200  p-4 m-4 h-full flex flex-col">
+    <div className="border border-primary-400 p-4 m-4 flex flex-col md:w-96">
       <div>
         <Link to={`/gift/${giftData.id}`}>
-          {/* <div
-            className="rounded-md relative overflow-hidden bg-white border-[1px]  border-primary-600 border-opacity-80"
-            style={{ paddingBottom: "100%" }}
-          > */}
-          
-          {/* Image */}
-          <div className=" p-4 md:p-10 flex flex-col justify-center">
+          <div className="p-4 flex justify-center ">
             <img
               src={giftData.img}
               alt={giftData.name}
-              className="lg:max-h-[60rem] md:max-h-[40rem] lg:p-18 md:p-14 p-10 rounded-md overflow-hidden shadow-lg  bg-slate-50 border border-primary-600 border-opacity-80"
+              className="w-full rounded-md overflow-hidden shadow-lg bg-slate-50 border border-primary-600 border-opacity-80 md:min-h-32"
             />
           </div>
         </Link>
       </div>
 
-      {/* Text */}
-      <div className="w-full flex flex-col justify-between p-4 md:p-10">
-        <div className="space-y-6 mb-6">
-          <h3 className="font-montserrat text-primary-100 text-h2 tracking-wider uppercase min-h-20 ">
+      <div className="flex flex-col justify-between p-4 md:p-6">
+
+        <div className="space-y-6 mb-6 flex-grow">
+          <h3 className="font-montserrat text-primary-100 text-h2 tracking-wider uppercase md:min-h-20 ">
             {giftData.name}
           </h3>
           <h3 className="font-montserrat  text-primary-100  text-[22px] tracking-wider">
             {giftData.price}
           </h3>
-          <p className="text-primary-100 text-p tracking-wider uppercase ">
+          <p className="text-primary-100 text-p tracking-wider uppercase md:min-h-60">
             {giftData.description}
           </p>
-        </div>
-        {/* Button */}
+       
+        
         <button
-          className="flex justify-center border rounded-md bg-white border-primary-200 text-primary-200 hover:bg-primary-200 hover:text-white font-montserrat text-h3 uppercase text-center my-2 w-full p-4 backdrop:tracking-wider"
+          className="flex justify-center border rounded-md bg-white border-primary-200 text-primary-200 hover:bg-primary-200 hover:text-white font-montserrat text-base md:text-lg uppercase text-center my-2 w-full p-3 md:p-4 backdrop-filter backdrop-blur-sm"
           onClick={() => {
             alert("You will be redirected to the purchase page.");
             window.open(giftData.source_url, "_blank");
@@ -51,6 +45,7 @@ function GiftComparisonCard(props) {
 
         <RemoveButton giftData={giftData} />
       </div>
+    </div>
     </div>
   );
 }
