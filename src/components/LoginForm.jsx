834b32219062d8ed0fc58904fa55b4e8835447c2
roberta de cecco <roberta.dececco@gmail.com>
2024-04-01 16:19:19 +0800

    styled the login form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -6,7 +6,7 @@ import { useAuth } from "../hooks/use-auth.js";
 
 function LoginForm() {
     const navigate = useNavigate();
-    const {setAuth} = useAuth();
+    const { setAuth } = useAuth();
 
     const [credentials, setCredentials] = useState({
         username: "",
@@ -27,7 +27,7 @@ function LoginForm() {
         if (credentials.username === "" || credentials.password === "") {
             alert("Please fill in all the fields");
             return;
-          };
+        };
 
         if (credentials.username && credentials.password) {
             postLogin(
@@ -47,30 +47,32 @@ function LoginForm() {
     };
 
     return (
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="username">Username: </label>
-                    <input
-                        type="text"
-                        id="username"
-                        placeholder="Enter username"
-                        onChange={handleChange}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="password">Password: </label>
-                    <input
-                        type="password"
-                        id="password"
-                        placeholder="Password"
-                        onChange={handleChange}
-                    />
-                </div>
-                <button 
-                    type="submit">
-                    Login
-                </button>
+        <div class="max-w-sm mx-auto bg-white shadow py-5 px-6">
+
+            <form>
+                <label class="block">
+                    <span class="block text-sm font-medium text-slate-700">Username</span>
+                    <input onChange={handleChange} id="username" type="text" placeholder="Enter your username" class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+            focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+            disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+            invalid:border-pink-500 invalid:text-pink-600
+            focus:invalid:border-pink-500 focus:invalid:ring-pink-500
+            "/>
+                    <span class="block text-sm font-medium text-slate-700">Password</span>
+                    <input onChange={handleChange} id="password" type="password" placeholder="Enter your password" class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+            focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+            disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+            invalid:border-pink-500 invalid:text-pink-600
+            focus:invalid:border-pink-500 focus:invalid:ring-pink-500
+            "/>
+                    <button className="flex justify-center border rounded-md bg-white border-primary-200 text-primary-200 hover:bg-primary-200 hover:text-white font-montserrat text-h3 uppercase text-center my-2 w-full p-4 backdrop:tracking-wider"
+                        onClick={handleSubmit}
+                    >
+                        LOGIN
+                    </button>
+                </label>
             </form>
+        </div>
     );
 }
 
