213b5c60f3350c526baa7141572fc66f6bcc4b52
cristianetanaka <147505024+cristianetanaka@users.noreply.github.com>
2024-04-03 17:26:10 +0800

    footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,11 +8,11 @@ function Footer() {
           className=""
         />
         {/* This needs to be fixed responsivness */}
-        <div className="absolute top-24 left-24 right-24 bottom-12 lg:top-60 lg:left-60 lg:right-60 lg:bottom-40 flex flex-col justify-center items-center bg-slate-100 gap-2 flex-auto mt-40 xl:my-60 xl:mx-80">
+        <div className="absolute top-22 left-9 right-9 bottom-3 lg:top-60 lg:left-60 lg:right-60 lg:bottom-45 flex flex-col justify-center items-center bg-slate-100 gap-2 xl:my-60 xl:mx-60">
           <img
             src="src/assets/Images/GIFT_COMPARE_LOGO.svg"
             alt="Footer Logo"
-            className="flex w-40 h-auto bg-slate-100 px-30 py-10"
+            className="w-40 h-35 bg-slate-100 px-14 py-4"
           />
           <p className="font-fredoka text-[12px] text-primary-200">Thank you for comparing your gifts with us!</p>
           <p className="font-montserrat text-[10px] text-primary-200 mb-2">Gift Compare Pty Ltd 2024</p>
