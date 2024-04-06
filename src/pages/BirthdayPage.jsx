0273c7db446842c1352ab6993e1646de5519270f
alisha <github@ahills.co>
2024-04-06 15:34:18 +0800

    Fixed import of useState lost due to merge conflicts and updated file path that was overwritten for the banner

--- src/pages/BirthdayPage.jsx
+++ src/pages/BirthdayPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import useGifts from "../hooks/use-gifts";
 import GiftCard from "../components/GiftCard";
 import PriceFilter from "../components/PriceFilter";
@@ -37,7 +38,7 @@ function BirthdayPage() {
     
   return (
       <div className="flex flex-col justify-center align-center space-y-8 m-12">
-          <div className="bg-[url('src/assets/Images/pexels-photo-796605.webp')] bg-cover bg-center flex justify-center items-center h-full">
+          <div className="bg-[url('assets/Images/pexels-photo-796605.webp')] bg-cover bg-center flex justify-center items-center h-full">
               <div className="bg-primary-300 px-6 my-14 md:mx-40 md:px-10 lg:px-20">
                   <h1 className="font-fredoka text-slate-100 text-h1 text-center tracking-wider uppercase px-1">
                       Birthday
