f06bc451b139e23d063c2aad8dc163177693638a
alisha <github@ahills.co>
2024-04-07 12:07:58 +0800

    updated Christmas with price filters

--- src/components/GiftCard.jsx
+++ src/components/GiftCard.jsx
@@ -53,7 +53,6 @@ function GiftCard(props) {
         </h3>
       </div>
       <h3 className="font-montserrat  text-primary-100  text-[22px] tracking-wider">
-        {giftData.id}
         ${giftData.price}
       </h3>
 
