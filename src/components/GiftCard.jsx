f26f5a49f82d5e6e35ca2c40153bc6a73986118b
alisha <github@ahills.co>
2024-04-07 11:49:28 +0800

    updated WeddingPage filters

--- src/components/GiftCard.jsx
+++ src/components/GiftCard.jsx
@@ -53,6 +53,7 @@ function GiftCard(props) {
         </h3>
       </div>
       <h3 className="font-montserrat  text-primary-100  text-[22px] tracking-wider">
+        {giftData.id}
         ${giftData.price}
       </h3>
 
